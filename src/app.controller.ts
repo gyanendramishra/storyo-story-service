853ea65a34b76b8bc0037a3f47eb2131f1a9b8e4
GYanendra Mishra <sourcecodist@gmail.com>
2023-05-02 00:58:31 +0530

    Story module implementaion in api gateway: initial commit

--- src/app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller()
-export class AppController {
-  @Get()
-  getHello(): string {
-    return 'Storyo gateway up and running';
-  }
-}
