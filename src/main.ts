845681a0ce0479293e688618ea7a5e2c5f155ed1
GYanendra Mishra <sourcecodist@gmail.com>
2023-05-02 00:58:31 +0530

    Story service: initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { INestMicroservice } from '@nestjs/common';
 import { MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import { grpcClientOptions } from './grpc-client-options';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    grpcClientOptions,
-  );
+  const app: INestMicroservice =
+    await NestFactory.createMicroservice<MicroserviceOptions>(
+      AppModule,
+      grpcClientOptions,
+    );
   await app.listen();
 }
 bootstrap();
